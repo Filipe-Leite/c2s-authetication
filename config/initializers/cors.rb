d8997a5aa4eaa08d2a099b018688eb7642e593d3
filipe-leite <filipedasilva94.1@gmail.com>
2024-08-10 23:22:21 -0300

    Ajusted cors config, routes config and pumas port config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000","http://localhost:3001","http://localhost:3002"
+    origins "http://localhost:3001",
+            "http://localhost:3002",
+            "http://localhost:3003",
+            "http://localhost:8000"
 
     resource "*",
       headers: :any,
